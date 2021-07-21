11a09a55e4cbd7ed25a082885ee3110448229b34
Keenan Carr <keenan.carr@oregonstate.edu>
2021-07-21 10:24:38 -0700

    Importing local module

--- SCRATCH.ps1
+++ SCRATCH.ps1
@@ -3,7 +3,7 @@
 AutoLoadENVs
 
 # Import the graph api token module
-Import-Module C:\Users\carrk\GitHub\Azure-AppService-GenerateSharePoint\Modules\New-GraphAPIToken\New-GraphAPIToken.psm1
+Import-Module .\Modules\New-GraphAPIToken\New-GraphAPIToken.psm1
 
 # Gathering a token and setting the headers
 $GraphAPIToken = New-GraphAPIToken -ClientID $env:ClientID -ClientSecret $env:ClientSecret -TenantID $env:TenantID
