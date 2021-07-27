5eb3273f43a03f58f4ecdebe31243c90b4ad8677
Keenan Carr <keenan.carr@oregonstate.edu>
2021-07-27 15:39:05 -0700

    updated scratch file

---
+++ NewSharePointSite/HelperFunctions.psm1
@@ -0,0 +1,45 @@
+function DoesOwnerExist {
+    <#
+    .SYNOPSIS
+    Check if the owner exists, return a true/false
+    #>
+    param (
+        $Headers,
+        $Owner
+    )
+    try {
+        $ownerprecheck = Invoke-WebRequest -Method Get -Headers $Headers -Uri "https://graph.microsoft.com/v1.0/users/$($Values.owner)"
+        return ($ownerprecheck.Content | ConvertFrom-Json).userprincipalname -eq $Values.owner
+    }
+    catch {
+        return $false    
+    }
+}
+
+<#
+.SYNOPSIS
+Find an unused mail nickname
+#>
+function FindUnusedMailNickname {
+    param (
+        $displayName,
+        $Headers
+    )
+        
+    $mailNickname = $( $displayName -replace '`~!@#$%^&*()-_=+?|/\;:,.<>', '' ).Replace(' ', '')
+    # Note we are adding the consistency level parameter to the headers so we can complete the group search against mail nicknames ( https://docs.microsoft.com/en-us/graph/api/group-list?view=graph-rest-1.0 )
+    # formatting the uri into the needed format: https://graph.microsoft.com/v1.0/groups?$search="mailNickname:<MAILNICKNAME>"
+    $test = Invoke-RestMethod -Method get -Headers $($Headers + @{ConsistencyLevel = 'eventual' }) -Uri $('https://graph.microsoft.com/v1.0/groups?$search=' + '"' + 'mailNickname:' + $mailNickname + '"')
+    $NicknameExists = $test.value.mailnickname -contains $mailNickname
+    if ($NicknameExists) {
+        $X = 1
+        do {
+            # Adding x to the nickname to try and find an unused nickname
+            $mailNickname = $( $displayName -replace '`~!@#$%^&*()-_=+?|/\;:,.<>', '' ).Replace(' ', '') + $X
+            $test = Invoke-RestMethod -Method get -Headers $($Headers + @{ConsistencyLevel = 'eventual' }) -Uri $('https://graph.microsoft.com/v1.0/groups?$search=' + '"' + 'mailNickname:' + $mailNickname + '"')
+            $NicknameExists = $test.value.mailnickname -contains $mailNickname
+            $X++
+        } until ($NicknameExists -eq $false)    
+    }
+    return $mailNickname
+}
--- SCRATCH.ps1
+++ SCRATCH.ps1
@@ -1,3 +1,14 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+if ($Request) {
+    $Request | Export-Clixml .\NewSharePointSite\request.cli.xml
+}
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
 # Load ENVs
 . C:\Users\carrk\GitHub\CodeSnippet-Azure-AutoLoadENVs\AutoLoadENVs.ps1
 AutoLoadENVs
@@ -5,22 +16,23 @@ AutoLoadENVs
 # Import the graph api token module
 Import-Module .\Modules\New-GraphAPIToken\New-GraphAPIToken.psm1
 
+# Import the helper functions
+Import-Module .\NewSharePointSite\HelperFunctions.psm1
+
 # Gathering a token and setting the headers
 $GraphAPIToken = New-GraphAPIToken -ClientID $env:ClientID -ClientSecret $env:ClientSecret -TenantID $env:TenantID
 $Headers = $GraphAPIToken.Headers
 
-# Setting the needed input values
-$owner = "carrk@oregonstate.edu"
-$displayName = "SharePoint Test"
-$description = "Testing SharePoint"
+# Importing the cached request
+$Request = Import-Clixml -Path .\NewSharePointSite\request.cli.xml
 
 # Creating the master 'values' variable that will contain all status information
 $Values = [PSCustomObject]@{
-    owner          = $owner
-    displayName    = $displayName
-    # Strip the special characters and spaces
-    description    = $description
-    
+    owner          = $Request.Body.owner
+    displayName    = $Request.Body.displayName
+    description    = $Request.Body.description
+    ticketID       = $Request.Body.ticketID
+
     # These will all be set later after other actions are taken
     ownerExists    = $null
     mailNickname   = $null
@@ -28,104 +40,106 @@ $Values = [PSCustomObject]@{
     creationstatus = $null
     Sharepointdata = $null
 }
-Write-Host "Owner detected as $($Values.owner)"
-Write-Host "Display name detected as $($Values.displayName)"
 
-# Precheck - does the owner exist?
-function DoesOwnerExist {
-    <#
-    .SYNOPSIS
-    Check if the owner exists, return a true/false
-    #>
-    param (
-        $Headers,
-        $Owner
-    )
-    try {
-        $ownerprecheck = Invoke-WebRequest -Method Get -Headers $Headers -Uri "https://graph.microsoft.com/v1.0/users/$($Values.owner)"
-        return ($ownerprecheck.Content | ConvertFrom-Json).userprincipalname -eq $Values.owner
+# Running precheck and gathering the needed information
+switch ($values) {
+    #Bad Outcomes
+    { [string]::IsNullOrEmpty($_.owner) } {
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::BadRequest
+                Body       = "Missing owner value"
+            })
+        continue
     }
-    catch {
-        return $false    
+    { [string]::IsNullOrEmpty($_.displayName) } {
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::BadRequest
+                Body       = "Missing displayName value"
+            })
+        continue
+    }
+    { [string]::IsNullOrEmpty($_.description) } {
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::BadRequest
+                Body       = "Missing description value"
+            })
+        continue
+    }
+    { [string]::IsNullOrEmpty($_.ticketID) } {
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::BadRequest
+                Body       = "Missing ticketID value"
+            })
+        continue
+    }
+    Default {
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode  = [HttpStatusCode]::BadRequest
+                Body        = "Request is in an invalid format"
+            })
+    }
+    #Good Outcomes
+    { ($_.owner -ne $null) -and ($_.displayName -ne $null) -and ($_.description -ne $null) -and ($_.ticketID -ne $null) } {
+        # Writing to the Azure Functions log stream.
+        Write-Host "Ticket ID detected as: $($Values.ticketID)"
+        Write-Host "Owner detected as: $($Values.owner)"
+        Write-Host "Display name detected as: $($Values.displayName)"
+
+        # Precheck - does the owner exist?
+        $values.ownerExists = $(DoesOwnerExist -Headers $Headers -Owner $Values.owner)
+
+        # Write to the Azure Functions log stream.
+        Write-Host "Owner exists: $($Values.ownerExists)"
+
+        # Precheck - Find an unused mail nickname
+        $Values.mailNickname = $(FindUnusedMailNickname -displayName $Values.displayName -Headers $Headers)
+
+        # Write to the Azure functions log stream.
+        Write-Host "Unused mail nickname: $($Values.mailNickname)"
     }
 }
 
-$values.ownerExists = $(DoesOwnerExist -Headers $Headers -Owner $Values.owner)
-
-Write-Host "Owner exists: $($Values.ownerExists)"
-
-
-
-
-# Precheck - is the mail nickname already in use?
-
-# Note we are adding the consistency level parameter to the headers so we can complete the group search against mail nicknames
-# https://docs.microsoft.com/en-us/graph/api/group-list?view=graph-rest-1.0
-# formatting the uri into the needed format: https://graph.microsoft.com/v1.0/groups?$search="mailNickname:<MAILNICKNAME>"
-
-
-function FindUnusedMailNickname {
-    param (
-        $displayName,
-        $Headers
-    )
-    
-    $mailNickname = $( $displayName -replace '`~!@#$%^&*()-_=+?|/\;:,.<>', '' ).Replace(' ', '')
-    $test = Invoke-RestMethod -Method get -Headers $($Headers + @{ConsistencyLevel = 'eventual' }) -Uri $('https://graph.microsoft.com/v1.0/groups?$search=' + '"' + 'mailNickname:' + $mailNickname + '"')
-    $NicknameExists = $test.value.mailnickname -contains $mailNickname
-    if ($NicknameExists) {
-        $X = 1
-        do {
-            $mailNickname = $( $displayName -replace '`~!@#$%^&*()-_=+?|/\;:,.<>', '' ).Replace(' ', '') + $X
-            $test = Invoke-RestMethod -Method get -Headers $($Headers + @{ConsistencyLevel = 'eventual' }) -Uri $('https://graph.microsoft.com/v1.0/groups?$search=' + '"' + 'mailNickname:' + $mailNickname + '"')
-            $NicknameExists = $test.value.mailnickname -contains $mailNickname
-            $X++
-        } until ($NicknameExists -eq $false)    
-    }
-    return $mailNickname
-}
-
-$Values.mailNickname = $(FindUnusedMailNickname -displayName $Values.displayName -Headers $Headers)
-
-
-
+# Creating the template to be used when creating the new sharepoint group
 switch ($Values) {
     # Bad outcomes
-    { $_.ownerExists -eq $false } { "Owner does not exist or could not be found"; continue }
     Default { "Could not determine the values status"; continue }
-
+    { $_.ownerExists -eq $false } { "Owner does not exist or could not be found"; continue }
+    { [string]::IsNullOrEmpty($_.mailNickname) } { "No mail nickname"; continue }
     # Good outcomes
-    { $_.ownerExists -eq $true } { "Everything is ready to roll"; continue }
+    { $_.ownerExists -eq $true } {
+        # Creating the template to be used for creating the new (sharepoint) group
+        # https://docs.microsoft.com/en-us/sharepoint/dev/solution-guidance/modern-experience-customizations-provisioning-sites
+        $values.template = [PSCustomObject]@{
+            description          = $Values.description
+            displayName          = $Values.displayName
+            groupTypes           = @("Unified")
+            mailEnabled          = $true
+            mailNickname         = $Values.mailNickname
+            securityEnabled      = $false
+            Visibility           = "private"
+            "owners@odata.bind"  = [array]@( $( [string]"https://graph.microsoft.com/v1.0/users/$($Values.owner)" ) )
+            "members@odata.bind" = [array]@( $( [string]"https://graph.microsoft.com/v1.0/users/$($Values.owner)" ) )
+        }
+    }
 }
 
+# If the template exists, make the new sharepoint group
+switch ($values) {
+    # Bad Outcomes
+    Default { "Could not determine the values status"; continue }
+    # Good Outcomes
+    { $_.template -ne $null } {
+        # Creating the sharepoint group and capturing the output
+        $values.creationStatus = Invoke-WebRequest -Method Post -Headers $Headers -ContentType 'application/json' -Uri "https://graph.microsoft.com/v1.0/groups" -Body $($values.template | ConvertTo-Json)
 
-# https://docs.microsoft.com/en-us/sharepoint/dev/solution-guidance/modern-experience-customizations-provisioning-sites
-$values.template = [PSCustomObject]@{
-    description          = $Values.description
-    displayName          = $Values.displayName
-    groupTypes           = @("Unified")
-    mailEnabled          = $true
-    mailNickname         = $Values.mailNickname
-    securityEnabled      = $false
-    Visibility           = "private"
-    "owners@odata.bind"  = [array]@( $( [string]"https://graph.microsoft.com/v1.0/users/$($Values.owner)" ) )
-    "members@odata.bind" = [array]@( $( [string]"https://graph.microsoft.com/v1.0/users/$($Values.owner)" ) )
+        # Waiting for one minute for replication
+        Start-sleep -Seconds 60
+
+        # Getting the sharepoint site information
+        $values.Sharepointdata = Invoke-RestMethod -Method get -Headers $Headers -Uri $('https://graph.microsoft.com/v1.0/sites?$search=' + '"' + $Values.displayName + '"') | Select-Object -ExpandProperty value | Where-Object { $_.name -eq $Values.mailNickname }
+
+    }
 }
 
-$values.creationStatus = Invoke-WebRequest -Method Post -Headers $Headers -ContentType 'application/json' -Uri "https://graph.microsoft.com/v1.0/groups" -Body $($values.template | ConvertTo-Json)
-
-Start-sleep -Seconds 60
-
-$values.Sharepointdata = Invoke-RestMethod -Method get -Headers $Headers -Uri 'https://graph.microsoft.com/v1.0/sites?$search="sharepoint test"' | Select-Object -ExpandProperty value | Where-Object { $_.name -eq $Values.mailNickname }
-
-Export-Clixml -InputObject $Values -Path .\values.cli.xml
-
-
-
-
-
-
-# $results = Invoke-RestMethod -Method get -Headers $Headers -Uri https://graph.microsoft.com/v1.0/sites/
-# $results2 = Invoke-RestMethod -Method get -Headers $Headers -Uri https://graph.microsoft.com/v1.0/sites/root
-# $results3 = Invoke-RestMethod -Method get -Headers $Headers -Uri 'https://graph.microsoft.com/v1.0/sites?$search="sharepoint test"'
-
+# Exporting the clixml with this run's values
+Export-Clixml -InputObject $Values -Path .\NewSharePointSite\values.cli.xml
